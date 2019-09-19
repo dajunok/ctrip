e9a29200f65c783518ad0a0f96b12a7c7a5ad282
liulijun <1937249556@qq.com>
2019-09-19 17:56:00 +0800

    提交：navbar.css

--- src/main/webapp/navbar.js
+++ src/main/webapp/navbar.js
@@ -3,21 +3,27 @@ var canvas=document.createElement("canvas");
 var nav_width=window.innerWidth;
 var x=window.screenX;
 var y=window.screenY;
-var first=document.getElementsByTagName("div")[1];  //获取body第一个div子节点
+var father=document.body;;  //获取body第一个div子节点
+var first=father.childNodes[1];
+
 canvas.setAttribute("id","nav-canvas");
 canvas.setAttribute("width",nav_width);
 canvas.setAttribute("height",37);
 //canvas.setAttribute("style","position:absolute;left:0px;top:0px;");
 //canvas.setAttribute("style","position:absolute;left:0px;top:0px;");
+//second.setAttribute("style","backgroundColor:red");
 //插入新节点
 
-body.insertBefore(canvas,first);
+father.insertBefore(canvas,first);
+first=father.childNodes[1];
 //body.appendChild(canvas);
 
 //绘制导航背景矩形
 var c=canvas;
 var ctx=c.getContext("2d");
-ctx.fillStyle="#F4F4F4";
+ctx.fillStyle="#F4F4F4";   //"#F4F4F4"
 ctx.fillRect(0,0,nav_width,37);
 
-//alert(canvas.id);
+
+//alert(first.nodeName);
+
