606ffeb337ca4904cfd91e84b6336a639909cace
liulijun <1937249556@qq.com>
2019-09-20 17:54:42 +0800

    提交：navbar.css navbar.js

--- src/main/webapp/navbar.js
+++ src/main/webapp/navbar.js
@@ -1,29 +1,6 @@
-﻿var body=document.body;
-var canvas=document.createElement("canvas");
-var nav_width=window.innerWidth;
-var x=window.screenX;
-var y=window.screenY;
-var father=document.body;;  //获取body第一个div子节点
-var first=father.childNodes[1];
+﻿var nav_width=window.innerWidth;  							//获取浏览器窗口宽度
+var div=document.getElementsByClassName("nav-bar-cont")[0];	
+div.setAttribute("style","width:"+nav_width+"px;");  		//设置导航栏背景宽度为浏览器窗口宽度
 
-canvas.setAttribute("id","nav-canvas");
-canvas.setAttribute("width",nav_width);
-canvas.setAttribute("height",37);
-//canvas.setAttribute("style","position:absolute;left:0px;top:0px;");
-//canvas.setAttribute("style","position:absolute;left:0px;top:0px;");
-//second.setAttribute("style","backgroundColor:red");
-//插入新节点
-
-father.insertBefore(canvas,first);
-first=father.childNodes[1];
-//body.appendChild(canvas);
-
-//绘制导航背景矩形
-var c=canvas;
-var ctx=c.getContext("2d");
-ctx.fillStyle="#F4F4F4";   //"#F4F4F4"
-ctx.fillRect(0,0,nav_width,37);
-
-
-//alert(first.nodeName);
+//alert(div.nodeName);
 
