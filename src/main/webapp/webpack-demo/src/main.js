b6221b10a1f66e75bb864b33a3d19b2b37219268
liulijun <1937249556@qq.com>
2019-12-24 17:55:29 +0800

    提交：webpack.config.js

--- src/main/webapp/webpack-demo/src/main.js
+++ src/main/webapp/webpack-demo/src/main.js
@@ -1,3 +1,5 @@
+//通过CommonJS规范导入CSS模块
+require('./main.css');
 //通过CommonJS规范导入show函数
 const show =require('./show.js');
 show('Webpack');
--- src/main/webapp/webpack-demo/webpack.config.js
+++ src/main/webapp/webpack-demo/webpack.config.js
@@ -1,5 +1,10 @@
 const path=require('path');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");          //webpack内置.js文件压缩插件
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");    //将css单独打包成一个文件的插件，它为每个包含css的js文件都创建一个css文件。它支持css和sourceMaps的按需加载。
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");  //该插件用于优化及压缩CSS资源
 module.exports={
+    // webpack会根据mode进行对Js打包，development压缩，production下面自动压缩，亲测没有问题
+    mode:"production", // production：生产模式； development：开发模式
     //JavaScript执行入口文件
     entry:'./src/main.js',
     output:{
@@ -7,5 +12,32 @@ module.exports={
       filename:'bundle.js',
       //将输出文件都放到dist目录下
       path:path.resolve(__dirname,'./dist')
-    }
+    },
+    module:{      
+      rules:[
+        {
+          //用正则表达式去匹配要用该Loader转换的CSS文件
+          test:/\.css$/,
+          use: [MiniCssExtractPlugin.loader,"css-loader"]
+        }
+      ]
+    },
+    optimization: {
+      minimizer: [
+        new UglifyJsPlugin({
+          cache: true,
+          parallel: true,
+          sourceMap: true // set to true if you want JS source maps
+        }),
+        new OptimizeCSSAssetsPlugin({})
+      ]
+    },
+    plugins:[
+        new MiniCssExtractPlugin({
+          // Options similar to the same options in webpackOptions.output
+          // both options are optional
+          filename: "[name].css",
+          chunkFilename: "[id].css"
+        })
+    ]
 };
