a00ea6679e464921ae5b945b8424633745ff5b8b
liulijun <1937249556@qq.com>
2019-12-25 18:00:12 +0800

    提交：webpack.config.js

--- src/main/webapp/webpack-demo/src/test.js
+++ src/main/webapp/webpack-demo/src/test.js
@@ -3,8 +3,10 @@ const Hello=require('./hello.js');
 const show=require('./show.js');
 
 console.log(__filename);
-console.log(__dirname);
-console.log(path.resolve(__dirname,'../dist'));
+console.log(__dirname);     //F:\SpringSource\projects\ctrip\src\main\webapp\webpack-demo\src
+console.log(path.resolve(__dirname,'../dist')); //F:\SpringSource\projects\ctrip\src\main\webapp\webpack-demo\dist
+console.log(path.resolve(__dirname,'app'));     //F:\SpringSource\projects\ctrip\src\main\webapp\webpack-demo\src\app
+console.log('[name]');
 hello = new Hello(); 
 hello.setName('BYVoid'); 
 hello.sayHello(); 
--- src/main/webapp/webpack-demo/webpack.config.js
+++ src/main/webapp/webpack-demo/webpack.config.js
@@ -3,21 +3,18 @@ const UglifyJsPlugin = require("uglifyjs-webpack-plugin");          //webpack内
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");    //将css单独打包成一个文件的插件，它为每个包含css的js文件都创建一个css文件。它支持css和sourceMaps的按需加载。
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");  //该插件用于优化及压缩CSS资源
 module.exports={
+    context:path.resolve(__dirname,'src'),   //基础目录，绝对路径，用于从配置中解析入口起点(entry point)和 loader
     // webpack会根据mode进行对Js打包，development压缩，production下面自动压缩，亲测没有问题
-    mode:"production", // production：生产模式； development：开发模式
-    //JavaScript执行入口文件
-    entry:'./src/main.js',
-    output:{
-      //将所有依赖的模块合并输出到一个bundle.js文件
-      filename:'bundle.js',
-      //将输出文件都放到dist目录下
-      path:path.resolve(__dirname,'./dist')
+    mode:"development", // production：生产模式； development：开发模式    
+    entry:'./main.js',  //JavaScript执行入口文件
+    output:{      
+      filename:'bundle.js',   //将所有依赖的模块合并输出到一个bundle.js文件      
+      path:path.resolve(__dirname,'./dist')   //将输出文件都放到dist目录下
     },
     module:{      
       rules:[
-        {
-          //用正则表达式去匹配要用该Loader转换的CSS文件
-          test:/\.css$/,
+        {          
+          test:/\.css$/,  //用正则表达式去匹配要用该Loader转换的CSS文件
           use: [MiniCssExtractPlugin.loader,"css-loader"]
         }
       ]
@@ -39,5 +36,12 @@ module.exports={
           filename: "[name].css",
           chunkFilename: "[id].css"
         })
-    ]
+    ],
+/*    devServer: {
+      //contentBase: path.join(__dirname, "dist"),
+      hot: true,
+      host:"192.168.254.100",
+      compress: true,
+      port: 9000
+    }*/
 };
